74de0ce064fea353f2d8277b5e78863f7d14e9db
Armando <amarti@mindden.com>
2019-11-12 18:06:01 +0100

    clean components: data access

---
+++ test.concepto/clean.components/src/User.DataAccess/Models/ApplicationUser.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity;
+using User.Domain.ValueObjects;
+
+namespace User.DataAccess.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public ApplicationUser()
+        {
+        }
+
+        public ApplicationUser(ShortName name, Email email, Password password)
+        {
+            this.UserName = name.ToString();
+            this.Email.ToString();                        
+        }
+    }
+}
