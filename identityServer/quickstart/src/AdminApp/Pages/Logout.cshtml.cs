cd0cf0d6c285ee222de223372b20ad82a358780e
Armando <amarti@mindden.com>
2019-11-14 12:12:04 +0100

    logout appadmin blazor server

---
+++ identityServer/quickstart/src/AdminApp/Pages/Logout.cshtml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AdminApp.Pages
+{
+    public class LogoutModel : PageModel
+    {
+        public async void OnGetAsync()
+        {
+            await HttpContext.SignOutAsync("Cookies");
+            var prop = new AuthenticationProperties()
+            {
+                RedirectUri = "/"
+            };
+            await HttpContext.SignOutAsync("oidc", prop);
+        }
+    }
+}
--- identityServer/quickstart/src/AppMvc/Controllers/HomeController.cs
+++ identityServer/quickstart/src/AppMvc/Controllers/HomeController.cs
@@ -49,7 +49,7 @@ namespace AppMvc.Controllers
             
             var client = new HttpClient();
             client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
-            var content = await client.GetStringAsync("http://localhost:5001/identity");
+            var content = await client.GetStringAsync("http://localhost:5001/identity");            
 
             ViewBag.Json = JArray.Parse(content).ToString();
             return View("json");
