dcf6ea6db03e5b1b7eb3428f9e6878d2d515cfb7
Armando <armando.marti.carretero@gmail.com>
2019-11-15 23:37:26 +0100

    inject dependences

--- test.concepto/clean.components/src/WebApi/DependencyInjection/UserInterfaceExtensions.cs
+++ test.concepto/clean.components/src/WebApi/DependencyInjection/UserInterfaceExtensions.cs
@@ -1,15 +1,19 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Application.Repositories;
+using Microsoft.Extensions.DependencyInjection;
 using WebApi.UseCases.GetUserDetails;
 
 namespace WebApi.DependencyInjection
 {
     public static class UserInterfaceExtensions
     {
-        
-        public static IServiceCollection AddPresenters(this IServiceCollection services)
+       public static IServiceCollection AddPresenters(this IServiceCollection services)
         {
             services.AddScoped<GetUserDetailsPresenter, GetUserDetailsPresenter>();
-            services.AddScoped<Application.Boundaries.GetUserDetails.IOutputPort>(x => x.GetRequiredService<GetUserDetailsPresenter>());
+            services.AddScoped<Application.Boundaries.GetUserDetails.IUseCase>(
+                ctx => new Application.UseCases.GetUserDetails(
+                    ctx.GetRequiredService<GetUserDetailsPresenter>(),
+                    ctx.GetRequiredService<IUserRepository>()
+                ));
 
             return services;
         }
