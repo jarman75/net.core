119aca2d6cd276d7cdb7744f0267b0f92838bf21
Armando <amarti@mindden.com>
2019-11-19 14:05:23 +0100

    proyecto con plantilla syncfusion .net core 3.1

---
+++ identityServer/quickstart/src/SyncfusionBlazorApp/Data/UserService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SyncfusionBlazorApp.Data
+{
+    public class UserService
+    {
+        private static readonly List<User> Users = new List<User>
+        {
+            new User  { Id = 1, Name = "Pablo Sanchez", Rol = "Administrador"},
+            new User  { Id = 2, Name = "Manuel Garcia", Rol = "Consultor"},
+            new User  { Id = 3, Name = "Rafael Gomez", Rol = "Editor"}
+        };
+
+        public Task<List<User>> GetUsersAsync()
+        {
+            return Task.FromResult(Users);            
+        }
+    }
+}
