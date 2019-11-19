fc90bf3f078183397342b17b2037a0bbaf499a92
Armando <amarti@mindden.com>
2019-11-19 14:29:06 +0100

    kata.inicial

--- katas/src/katas.tests/RefactoringTests.cs
+++ katas/src/katas.tests/RefactoringTests.cs
@@ -1,7 +1,68 @@
+using System;
 using NUnit.Framework;
 
 namespace katas.tests
 {
+    /*
+    
+    Refactorizar clase Kata para quitar el switch. 
+    Cambiar enum 'Status' por:
+    
+    public abstract class Status
+    {
+        public abstract string GetStatusDescription();
+    }
+
+    El código de los test no se puede modificar
+
+    */
+
+     public class Kata
+    {
+        public enum Status
+        {
+            Default = 0,
+            New = 1,
+            Active = 2,
+            Deactivated = 3
+        }
+    
+        private readonly Status _status;
+
+        public Kata()
+        {
+        }
+
+        public Kata(Status status)
+        {
+            _status = status;
+        }
+
+        public string GetStatusDescription()
+        {
+            switch (_status)
+            {
+                case Status.Default:
+                    return "I have never been set";
+
+                case Status.New:
+                    return "I am new!";
+
+                case Status.Active:
+                    return "I am active";
+
+                case Status.Deactivated:
+                    return "I have been deactivated";
+
+                default:
+                    throw new InvalidOperationException("Invalid status encountered");
+            }
+        }
+    }
+
+    /***************************************************************
+                TESTS
+    ***************************************************************/
     public class RefacotingTests
     {
         [Test]
