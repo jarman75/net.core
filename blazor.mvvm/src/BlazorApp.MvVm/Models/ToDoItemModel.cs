d702ad5b355ce7d01cda64e2ad5cd4b71563334c
Armando Martí <amarti@mindden.com>
2020-12-23 15:03:24 +0100

    validacion

--- blazor.mvvm/src/BlazorApp.MvVm/Models/ToDoItemModel.cs
+++ blazor.mvvm/src/BlazorApp.MvVm/Models/ToDoItemModel.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace BlazorApp.MvVm.Models
 {
@@ -7,6 +8,7 @@ namespace BlazorApp.MvVm.Models
         public Guid Id { get; set; }
         public bool Done { get; set; }
         public DateTime? Date { get; set; }
+        [Required(ErrorMessage ="Esto es obligatorio capullo")]
         public string Title { get; set; }
         public string Notes { get; set; }
     }
