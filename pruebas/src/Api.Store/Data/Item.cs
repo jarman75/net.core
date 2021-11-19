f1f8d4887ea7848113e886f16190d4c1f148c05f
Armando Martí <amarti@mindden.com>
2021-11-19 20:41:20 +0100

    prueba store

---
+++ pruebas/src/Api.Store/Data/Item.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Api.Store.Data
+{
+    public class Item
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public Category Category { get; set; }                
+        public List<ItemStock> Stocks { get; set; }
+
+    }
+    public class ItemStock
+    {
+        public Guid Id { get; set; }
+        public Guid ItemId { get; set; }
+        public DateTime? ExpirationDate { get; set; }
+        public DateTime? ManufacturingDate { get; set; }
+        public DateTime? Entrydate { get; set; }
+        public double CostPrice { get; set; }
+        public double Price { get; set; }
+
+    }
+
+    public enum Category
+    {        
+        Normal = 0,        
+        Aged = 1,        
+        Perishable = 2
+    }
+}
